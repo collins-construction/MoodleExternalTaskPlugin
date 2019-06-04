80cadb13d81d7a37d7f4682381c6ca736b68ccde
collinsconstruction <47786614+collinsconstruction@users.noreply.github.com>
2019-06-04 10:29:43 +0100

    Updated copyright

--- lang/en/qtype_externaltask.php
+++ lang/en/qtype_externaltask.php
@@ -19,7 +19,8 @@
  *
  * @package    qtype
  * @subpackage externaltask
- * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @copyright  2019 Collins Construction
+ * @copyright  based on work by 1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
